b2aa7f007de9877b806e8c5f563b657eeae6f70c
terterfred <fred545915853@hotmail.com>
2015-11-13 17:40:31 -0600

    no message

---
+++ 01 Data/eft.R
@@ -0,0 +1,46 @@
+setwd("/Users/Fred/DV_RProject4/03 Data")
+
+file_path <- "air_quality.csv"
+
+df <- read.csv(file_path, stringsAsFactors = FALSE)
+
+# Replace "." (i.e., period) with "_" in the column names.
+names(df) <- gsub("\\.+", "_", names(df))
+
+str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
+
+
+measures <- c("End_Date", "End_Time", "PM10", "NO2")
+
+library(lubridate)
+require(dplyr)
+# Fix date columns, this needs to be done by hand because | needs to be correct.
+#                                                        \_/
+df<- df %>% mutate(End_Date = parse_date_time(End_Date, "%d%m%y"))
+
+
+#df$End_Date <- data.frame(lapply(df[0], gsub, pattern="-",replacement= "/"))
+
+# The following is an example of dealing with special cases like making state abbreviations be all upper case.
+# df["State"] <- data.frame(lapply(df["State"], toupper))
+
+# Get rid of all characters in measures except for numbers, the - sign, and period.dimensions
+
+
+write.csv(df, paste(gsub(".csv", "", file_path), ".reformatted.csv", sep=""), row.names=FALSE, na = "")
+
+tableName <- gsub(" +", "_", gsub("[^A-z, 0-9, ]", "", gsub(".csv", "", file_path)))
+sql <- paste("CREATE TABLE", tableName, "(\n-- Change table_name to the table name you want.\n")
+if( length(measures) > 1 || ! is.na(dimensions)) {
+  for(d in dimensions) {
+    sql <- paste(sql, paste(d, "varchar2(4000),\n"))
+  }
+}
+if( length(measures) > 1 || ! is.na(measures)) {
+  for(m in measures) {
+    if(m != tail(measures, n=1)) sql <- paste(sql, paste(m, "number(38,4),\n"))
+    else sql <- paste(sql, paste(m, "number(38,4)\n"))
+  }
+}
+sql <- paste(sql, ");")
+cat(sql)
